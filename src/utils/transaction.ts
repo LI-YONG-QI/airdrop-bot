1f6a831f7f09cbc4c6a6c0707a72ac2db46ccccb
LI-YONG-QI <a2369028@gmail.com>
2024-05-03 01:24:53 +0800

    fix: reverted error

--- src/utils/transaction.ts
+++ src/utils/transaction.ts
@@ -2,13 +2,19 @@ import { WalletClient } from "viem";
 import { PUBLIC_CLIENT } from "./clients/public";
 
 export async function sendTransaction(request: any, signer: WalletClient) {
-  const hash = await signer.writeContract(request);
-  const transaction = await PUBLIC_CLIENT.waitForTransactionReceipt({
-    confirmations: 2,
-    hash,
-  });
+  while (1) {
+    const hash = await signer.writeContract(request);
+    const transaction = await PUBLIC_CLIENT.waitForTransactionReceipt({
+      confirmations: 5,
+      hash,
+    });
 
-  console.log(
-    `Tx Hash: ${transaction.transactionHash} - ${transaction.status}`
-  );
+    console.log(
+      `Tx Hash: ${transaction.transactionHash} - ${transaction.status}`
+    );
+
+    if (transaction.status === "success") break;
+
+    console.log("Reverted !! Retrying...");
+  }
 }
